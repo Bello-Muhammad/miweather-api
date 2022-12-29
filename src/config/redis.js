3a4607281d99f7fca17a0e7753d8ea9bb48268c3
Bello-Muhammad <bellob253@gmail.com>
2022-12-29 11:51:49 +0100

    new

--- src/config/redis.js
+++ src/config/redis.js
@@ -1,8 +1,14 @@
 const redis = require('redis')
+const Redis = require('ioredis');
+const fs = require('fs');
 
-const port = process.env.REDIS_PORT || 6379
+// const port = process.env.REDIS_PORT || 6379
 
-const redisClient = redis.createClient(port);
+const redisClient = redis.createClient({
+    host: 'redis-14321.c15.us-east-1-4.ec2.cloud.redislabs.com',
+    port: 14321,
+    password: process.env.REDIS-PWD
+});
 
 redisClient.connect()
 
@@ -10,6 +16,9 @@ redisClient.on('error', (err) => {
     console.log(err)
 })
 
+
+
+// const redis = new Redis();
 
 
 module.exports = redisClient;
