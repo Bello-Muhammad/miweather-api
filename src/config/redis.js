90eb2ab214c3e080cdbd41ce5bb4df4f4ec4689b
Bello Muhammad <67134404+Bello-Muhammad@users.noreply.github.com>
2023-09-13 16:35:45 +0100

    Update redis.js

--- src/config/redis.js
+++ src/config/redis.js
@@ -14,17 +14,3 @@ redisClient.on('error', (err) => {
 
 
 module.exports = redisClient;
-
-
-
-
-// let redisClient;
-
-
-// (async () => {
-//   redisClient = redis.createClient();
-
-//   redisClient.on("error", (error) => console.error(`Error: ${error}`));
-
-//   await redisClient.connect();
-// })();
