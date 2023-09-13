8004d0a2d02a394a53ce8c2768dbb9c8a3d8bfaf
Bello Muhammad <67134404+Bello-Muhammad@users.noreply.github.com>
2023-09-13 16:02:27 +0100

    Update cached.js

--- src/middleware/cached.js
+++ src/middleware/cached.js
@@ -2,12 +2,11 @@ const Client = require('../config/redis')
 
 const cacheData = async(req, res, next) => {
     const city = req.body.city
-    let details
     try {
         
         const cacheResult = await Client.get(city)
         if(cacheResult) {
-            details = JSON.stringify(cacheResult);
+            let details = JSON.stringify(cacheResult);
             res.render('weather', {location: details})
         }else{
             next()
